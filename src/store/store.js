58e4dd5981e3ae5d7c7794bf2e8c19e6cb78c2ed
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-07 15:15:42 +0530

    added new reducer to show connections

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from '../features/userSlice'
 import userFeed from '../features/feedSlice'
+import userConnections from '../features/connectionsSlice'
 
 const store = configureStore({
     reducer :{
        user: userReducer,
        feed: userFeed,
+       connections:userConnections
     }
 }) 
 
