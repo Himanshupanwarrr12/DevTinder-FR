e80983534d13acc71dcc34d66d6bee62fb4605d9
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-10 03:33:54 +0530

    feat:to send/ignore user from the feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import axios from "axios";
+import { baseUrl } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../features/feedSlice";
 
 function UserCard({ user }) {
-  if(!user) return <div>No user data available</div>
-  const { firstName, photoUrl, about } = user;
+  const dispatch = useDispatch();
+  if (!user) return <div>No user data available</div>;
+  const { _id, firstName, photoUrl, about } = user;
+
+  const handleFeedAction = async (status, userId) => {
+    try {
+      await axios.post(
+        baseUrl + "/request/send/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUserFromFeed(userId))
+    } catch (error) {
+      console.log("Error :", error);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center border rounded bg-gray-700">
@@ -15,21 +32,17 @@ function UserCard({ user }) {
           />
         </figure>
         <div className="card-body w-full bg-gray-800 p-3">
-          {/* Added break-words and line-clamp */}
           <p className="card-title text-white text-xl font-semibold mb-2 break-words line-clamp-1">
             {firstName}
           </p>
-          
-          {/* Added break-words and line-clamp with scroll */}
+
           <div className="max-h-20 overflow-y-auto mb-2">
-            <p className="text-white font-semibold  break-words">
-              {about}
-            </p>
+            <p className="text-white font-semibold  break-words">{about}</p>
           </div>
-          
+
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Ignore</button>
-            <button className="btn btn-secondary">Interested</button>
+            <button className="btn btn-primary" onClick={()=> handleFeedAction("ignored",_id)} >Ignore</button>
+            <button className="btn btn-secondary" onClick={()=> handleFeedAction("interested",_id)} >Interested</button>
           </div>
         </div>
       </div>
